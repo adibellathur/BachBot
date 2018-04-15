a928b923fec4508e9ad4eb40f59055f2c0932d51
wakugawa <wakugawa@usc.edu>
2018-04-15 13:32:55 -0700

    added userbool object

--- BackEnd/FinalProject-Testing/src/model/User.java
+++ BackEnd/FinalProject-Testing/src/model/User.java
@@ -3,5 +3,9 @@ package model;
 public class User {
 	public String username;
 	public String imageUrl;
-	public String followers;
+	
+	public User(String username, String imageUrl) {
+		this.username = username;
+		this.imageUrl = imageUrl;
+	}
 }
---
+++ BackEnd/FinalProject-Testing/src/model/UserBool.java
@@ -0,0 +1,11 @@
+package model;
+
+public class UserBool {
+	private boolean exists;
+	private User user;
+	
+	public UserBool(boolean exists, User user) {
+		this.exists = exists;
+		this.user = user;
+	}
+}
