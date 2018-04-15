5b56d94fb5f17c3bdb75b3436fb800ea02cce4a4
Adithya Bellathur <bellathu@usc.edu>
2018-04-15 13:18:28 -0700

    Update 4-15: added search functionality, true api call handling

--- FrontEnd/js/search.js
+++ FrontEnd/js/search.js
@@ -1,17 +1,19 @@
-function validateSearch(form) {
-  var search = document.getElementById("searchbar").value;
+function validateSearch(num) {
+  var search = document.getElementById("searchbar-input-" + num).value;
   console.log("search = " + search);
   var params = {
     "search": search
   }
   getSearchResults(params);
-  sessionStorage.setItem("view", "search")
-  //loadContent();
+  sessionStorage.setItem("view", "Search");
+  loadContent();
   return false;
 }
 
 function getSearchResults(params) {
   console.log(params);
+  document.getElementById("you-searched-for").innerHTML = "<h2>You Searched for \"" +
+                                                          params.search + "\""
   // $.ajax({
   //   url: '',
   //   data: { format: 'json' },
